3557f8392ac77d697db0c154a5f6bc832ad5a1ce
Travis Jeffery <tj@travisjeffery.com>
2014-09-23 23:56:29 -0500

    Add usage

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import "github.com/visionmedia/docopt"
+import "github.com/segmentio/go-log"
+import "fmt"
 
 const Version = "0.0.1"
 
-func main() {
+const Usage = `
+  Usage:
+   versions search <name>
+   versions register <name>
+   versions -h | --help
+   versions --version
 
+  Options:
+    -h, --help       output help information
+    -v, --version    output version
+`
+
+func main() {
+	args, err := docopt.Parse(Usage, nil, true, Version, false)
+	log.Check(err)
+
+	name := args["<name>"].(string)
+
+	fmt.Println(name)
+
+	if args["search"].(bool) {
+
+	} else if args["register"].(bool) {
+
+	}
 }
